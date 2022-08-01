ae12352eaf70a832bf7286e6075797b88a611d0a
GUI_nephelo <1736319094@qq.com>
2022-08-01 22:59:40 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="ononon">123123</button>
+  </div>
+</template>
+
+<script>
+import {getContext} from '../api/apiHelper.js'
+       
+export default {
+    name: 'HeaDer',
+    methods: {
+        ononon: function(){
+            getContext().then(alert)
+        }
+
+    }
+}
+
+</script>
